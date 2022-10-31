6ad274fa008caded95eb0476547f18e2c21d4608
sungseokmin <jkl154527@gmail.com>
2022-10-31 11:47:59 +0900

    🚚  Part1. 페이지 & 파일 기반 라우팅

--- pages/clients/[id]/[clientProjectId].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const SelectedClientProjectPage = () => {
-  const router = useRouter();
-
-  return <div>Selected Client Project Page</div>;
-};
-
-export default SelectedClientProjectPage;
--- pages/portfolio/list/index.js
+++
@@ -1,5 +0,0 @@
-const PortfolioListPage = () => {
-  return <div>PortfolioList Page</div>;
-};
-
-export default PortfolioListPage;
