311a552281aedbf9085190303a6d2d6ba5f3eb70
sungseokmin <jkl154527@gmail.com>
2022-11-03 11:03:20 +0900

    📝 part3. 페이지 사전 렌더링 & 데이터 페칭 (50%)

--- part3. 페이지 사전 렌더링 & 데이터 페칭/pages/[productId].js
+++ part3. 페이지 사전 렌더링 & 데이터 페칭/pages/[productId].js
@@ -4,7 +4,12 @@ import fs from 'fs/promises';
 import { Fragment } from 'react';
 
 const ProductDetailPage = ({ product }) => {
+  if (!product) {
+    return <p>Loading ... </p>;
+  }
+
   const { title, description } = product;
+
   return (
     <Fragment>
       <h1>{title}</h1>
@@ -15,19 +20,43 @@ const ProductDetailPage = ({ product }) => {
 
 export default ProductDetailPage;
 
-export const getStaticProps = async (context) => {
-  const { params } = context;
-  const { productId } = params;
-
+const getData = async () => {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
 
   const { products } = JSON.parse(jsonData);
 
+  return products;
+};
+
+export const getStaticPaths = async () => {
+  const products = await getData();
+
+  const paths = products.map((product) => ({
+    params: { productId: product.id },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const { productId } = params;
+
+  const products = await getData();
+
   const product = products.find((product) => product.id === productId);
+
+  if (!product) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { product },
-    revalidate: 2, // 10초
   };
 };
